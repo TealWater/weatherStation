b74830202fb6e32201990601b5144a1430ff4098
Matthew Straughn <matthewe99@hotmail.com>
2019-10-18 14:40:35 -0400

    formatted code

--- src/app/geoSearch.java
+++ src/app/geoSearch.java
@@ -17,7 +17,8 @@ import org.json.JSONObject;
 public class geoSearch {
     public static void main(String[] args) {
         
-        urlEncoder("Geneva", "NY");
+        //urlEncoder("Geneva", "NY");
+        geoEncoder();
 
     }//end of main
 
@@ -34,7 +35,7 @@ public class geoSearch {
     }//end of urlEncoder()
     
 
-        public void geoEncoder(){
+        public static void geoEncoder(){
         try {
         URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCPAYQsQaQwH4rD55sBNBpJtSP1r6jJSqk"); //url we want to talk to
         HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // opens connection and casts it to type HTTPURLConnection (makes it more secure?)
